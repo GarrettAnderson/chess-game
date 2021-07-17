a6876322fc62dc5ea8366295266dec7aba4a2d38
Garrett Anderson <garrettleegrahamanderson@gmail.com>
2021-07-17 18:02:36 -0400

    created some variables for the white and black pawns and remaining pieces

--- public/main.js
+++ public/main.js
@@ -1,7 +1,23 @@
-function main() {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
-}
+const board = document.getElementsByClassName('chessboard-container')
+console.log(board)
 
-document.addEventListener('DOMContentLoaded', main)
+// create variables for the white and black pieces
+// create a function that creates the board
+
+const whitePawn = '&#9817;'
+const whiteMonarchs = [
+  {rank: 'rook', hex: '&#9814;'},
+  {rank: 'knight', hex: '&#9816;'},
+  {rank: 'bishop', hex: '&#9815;'},
+  {rank: 'queen', hex: '&#9813;'},
+  {rank: 'king', hex: '&#9812;'}
+]
+
+const blkPawn = '&#9823;'
+const blkMonarchs = [
+  {rank: 'rook', hex: '&#9820;'},
+  {rank: 'knight', hex: '&#9822;'},
+  {rank: 'bishop', hex: '&#9821;'},
+  {rank: 'queen', hex: '&#9819;'},
+  {rank: 'king', hex: '&#9818;'}
+]
